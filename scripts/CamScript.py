606dbe5d9e7f01ec52c425debacab4fb8924b162
TobiasK98 <tobias.koschorreck@web.de>
2021-02-25 19:31:50 +0100

    improved CamScript

--- scripts/CamScript.py
+++ scripts/CamScript.py
@@ -3,9 +3,11 @@ import os
 import tensorflow as tf
 import pathlib
 import glob
+import matplotlib.pyplot as plt
 from object_detection.utils import ops as utils_ops
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
+from google.colab.patches import cv2_imshow
 from operator import itemgetter, attrgetter
 from PIL import Image
 
@@ -14,10 +16,11 @@ from PIL import Image
 while "models" in pathlib.Path.cwd().parts:
     os.chdir('..')
 
+# function for loading our speed sign pictures for displaying the current speed
 def load_images(): 
 
     image_list = []
-    for filename in glob.glob('/home/tobias/Studium/GdKi/Speed_signs/*.jpg'):
+    for filename in glob.glob('/content/gdrive/MyDrive/Speed_signs/*.jpg'):
         im=Image.open(filename)
         image_list.append((filename, im))
     return image_list
@@ -25,7 +28,7 @@ def load_images():
 
 def load_model(model_name):
     # Provide the path where the trained model directory is located
-    model_dir = "/home/tobias/Studium/GdKi/my_ssd_resnet50_v1_fpn_2/Tensorflow/workspace/training_demo/exported-models/" + model_name
+    model_dir = "/content/gdrive/MyDrive/" + model_name
     model_dir = pathlib.Path(model_dir) / "saved_model"
 
     model = tf.saved_model.load(str(model_dir))
@@ -33,7 +36,7 @@ def load_model(model_name):
     return model
 
 
-PATH_TO_LABELS = '/home/tobias/Studium/GdKi/GitSetupColab/workspace/training_demo/annotations/label_map.pbtxt'
+PATH_TO_LABELS = '/content/Tensorflow/workspace/training_demo/annotations/label_map.pbtxt'
 category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
 
 model_name = "my_ssd_resnet50_v1_fpn"
@@ -101,8 +104,10 @@ def show_inference(model, frame):
 # Now we open the webcam and start detecting objects
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("/content/gdrive/MyDrive/TestVideo7.mp4")
 
+#create VideoWriter Object
+out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 30, (1280, 720))
 
 def run_inference(model, cap):
     image_list = load_images()
@@ -110,63 +115,73 @@ def run_inference(model, cap):
     while cap.isOpened():
 
         ret, image_np = cap.read()
-        # Actual detection.
-        output_dict = run_inference_for_single_image(model, image_np)
 
-        #Create indexes list of element with a score > 0.5
-        indexes = [k for k,v in enumerate(output_dict['detection_scores']) if (v > 0.5)]
-        
-        #Number of entities
-        num_entities = len(indexes)
-        
-        #Extract the class id
-        class_id = itemgetter(indexes)(output_dict['detection_classes'])
-        scores = itemgetter(indexes)(output_dict['detection_scores'])
-       
-        #Convert the class id in their name
+        if ret:
+            # Actual detection.
+            output_dict = run_inference_for_single_image(model, image_np)
 
-        class_names = []
+            #Create indexes list of element with a score > 0.5
+            indexes = [k for k,v in enumerate(output_dict['detection_scores']) if (v > 0.5)]
+            
+            #Number of entities
+            num_entities = len(indexes)
+            
+            #Extract the class id
+            class_id = itemgetter(indexes)(output_dict['detection_classes'])
+            scores = itemgetter(indexes)(output_dict['detection_scores'])
+          
+            #Convert the class id in their name
+            class_names = []
 
-        if num_entities == 1:
-            class_names.append(category_index[class_id[0]]['name'])
-            class_name = str(class_names)
-        else:
-            for i in range(len(indexes)):
-                class_names.append(category_index[class_id[i]]['name'])
-                            
-        
+            if num_entities == 1:
+                class_names.append(category_index[class_id[0]]['name'])
+                class_name = str(class_names)
+            else:
+                for i in range(len(indexes)):
+                    class_names.append(category_index[class_id[i]]['name'])
+                                
+            
 
-        # Visualization of the results of a detection.
-        vis_util.visualize_boxes_and_labels_on_image_array(
-            image_np,
-            output_dict['detection_boxes'],
-            output_dict['detection_classes'],
-            output_dict['detection_scores'],
-            category_index,
-            instance_masks=output_dict.get('detection_masks_reframed', None),
-            use_normalized_coordinates=True,
-            line_thickness=8)
+            # Visualization of the results of a detection.
+            vis_util.visualize_boxes_and_labels_on_image_array(
+                image_np,
+                output_dict['detection_boxes'],
+                output_dict['detection_classes'],
+                output_dict['detection_scores'],
+                category_index,
+                instance_masks=output_dict.get('detection_masks_reframed', None),
+                use_normalized_coordinates=True,
+                line_thickness=8)
 
-        image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
-        cam_im = Image.fromarray(image_np)
-        for j in range(len(class_names)):
-            for k in range(len(image_list)):
-                if(class_names[j] == image_list[k][0].split("/")[-1][:-4]):
-                    #image = cv2.imread("/home/tobias/Studium/GdKi/Speed_signs/" + image_list[k])
-                    speed = image_list[k][1]
-                    speed = speed.resize((100, 100))
-                    break
-        
-        if(speed):
-            cam_im.paste(speed,(0,0))
+            image_np = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
 
-        image_np = np.array(cam_im)
-        image_np = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
-        cv2.imshow('object_detection', cv2.resize(image_np, (800, 600)))
-        if cv2.waitKey(25) & 0xFF == ord('q'):
-            cap.release()
-            cv2.destroyAllWindows()
+            # getting the png for our prediction
+            cam_im = Image.fromarray(image_np)
+            for j in range(len(class_names)):
+                for k in range(len(image_list)):
+                    if(class_names[j] == image_list[k][0].split("/")[-1][:-4]):
+                        speed = image_list[k][1]
+                        speed = speed.resize((100, 100))
+                        break
+            
+            # pasting the current speed into our picture(numpyarray)
+            if(speed):
+                cam_im.paste(speed,(0,0))
+
+            image_np = np.array(cam_im)
+            image_np = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
+            
+            # writing our video frame by frame into a new file
+            out.write(image_np)
+
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                cap.release()
+                out.release()
+                cv2.destroyAllWindows()
+                break
+        else: 
             break
-
-
+            
+    cap.release()
+    out.release()
 run_inference(detection_model, cap)
